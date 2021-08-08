2ef5bc43403f77fcb023622c497f2dbe14031fd5
Percival Lucena <plucena@br.ibm.com@Percivals-MacBook-Pro-2.local>
2021-08-08 08:45:24 -0300

    first commit

---
+++ src/main/java/com/example/demo/repos/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+}
+
